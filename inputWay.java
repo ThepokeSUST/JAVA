4206c7d97cb6de2d187c42fc269bf2e694ea2a79
dipok <dipoknathdipu@gmail.com>
2025-01-09 11:46:02 +0600

    scanner class

--- inputWay.java
+++ inputWay.java
@@ -1,13 +1,19 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class inputWay {
     
     public static void main(String[] arg)throws IOException{
-          
+        //via scanner class
+        /*Scanner cc= new Scanner(System.in);
+        int a= cc.nextInt();
+        //for string (next() or nextLine())
+        System.out.println(a);  
+        */
         //via console
         /*String str= System.console().readLine("enter");
         System.out.println(str);
