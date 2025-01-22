acff36ff5db47ed4c34c44a13a3792b8b197310c
dipok <dipoknathdipu@gmail.com>
2025-01-22 13:07:23 +0600

    before oop

--- AbstractMethod.java
+++ AbstractMethod.java
@@ -1,36 +1,36 @@
 
-abstract class Parant1{
-       
-    public Parant1(){
+abstract class Parant1 {
+
+    public Parant1() {
         System.out.println("this is Base1 class constructor");
     }
 
-   public abstract void greet();
-    public void dis(){
+    public abstract void greet();
+
+    public void dis() {
         System.out.println("this is parant1");
     }
 }
 
- class Child2 extends Parant1{
-      
-     public Child2(){
+class Child2 extends Parant1 {
+
+    public Child2() {
         System.out.println("constructor of Child 2 class");
-     }
+    }
+
     // public abstract void greet();
     @Override
-    public void greet(){
+    public void greet() {
         System.out.println("namashte from child 2");
     }
 }
 
 public class AbstractMethod {
-    
-    
-    public static void main(String[] arg){
-        Child2 ch= new Child2();
+
+    public static void main(String[] arg) {
+        Child2 ch = new Child2();
         ch.greet();
-       ch.dis();
+        ch.dis();
     }
-   
-    
+
 }
---
+++ before_OPP/dataTypes.java
@@ -0,0 +1,43 @@
+package before_OPP;
+
+public class dataTypes {
+    // java is statically typed and also strongly typed language ...
+
+    // statically typed...........
+    // A statically typed lamguage is a language where the type of variables are
+    // known a compile time rather than run time.This meanns that the programmer
+    // must declare the types of their data explicitly in their code...
+
+    // strongly typed..............
+    // A strongly typed programming language is a language that strictly defines the
+    // type of each variable and value during compilation. This means that the
+    // compiler can detect errors related to incorrect variable types during
+    // compilation and execution.
+
+    // Java has two categories in which data types are segregated.............
+
+    // primitive data type in java
+    // Primitive data are only single values and have no special capabilities. There
+    // are 8 primitive data types.
+    // boolean 8bits true->false default = false
+    /*
+     * The boolean data type represents a logical value that can be either true or
+     * false. Conceptually, it represents a single bit of information, but the
+     * actual size used by the virtual machine is implementation-dependent and
+     * typically at least one byte (eight bits) in practice. Values of the boolean
+     * type are not implicitly or explicitly converted to any other type using
+     * casts.
+     * 
+     */
+    // byte 8bits -128->127 default=0
+    // char 16bits
+    // short 16bits -32768 -> 32767
+    // int 32bits -2147483648->2147483647
+    // long 64bits -2^32->2^32 -1
+    // float 32bits
+    // double 64bits
+
+    //Non primitive data types
+    
+
+}
