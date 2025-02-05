27487d54c4eb6c0b74d3b0a87ebc54d4a250e0b6
dipok <dipoknathdipu@gmail.com>
2025-02-05 17:18:26 +0600

    data types

---
+++ Datatypes.java
@@ -0,0 +1,19 @@
+public class Datatypes {
+
+    public static void main(String[] arg) {
+
+        // Data types
+        // it is the type of the data that can store in the variable that is declared
+        // follows by that data type.
+        // data type defines that what amount of memory can store a variable.
+
+        // data type specify what kind of data a variable can store.
+
+        // java has two categories of data type.
+
+        // primitive data types.
+        // there are eight primitive types.
+        // the primitive types are also referred to as simple types.
+        // Primitive types represents single values,it has no extra capabilities
+    }
+}
