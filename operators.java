634c2417e1f9c73b6801bd958a084936059f0942
dipok <dipoknathdipu@gmail.com>
2025-02-13 01:20:31 +0600

    operator

---
+++ operators.java
@@ -0,0 +1,81 @@
+public class operators {
+    
+
+    public static void main(String[] args) {
+        
+        /*
+         Java operators are special symbols that perform operations on variables or values. They can be classified into several categories based on their functionality. These operators play a crucial role in performing arithmetic, logical, relational, and bitwise operations etc.
+         */
+
+         /*
+          Types of Operators in Java
+             Arithmetic Operators
+             Unary Operators
+             Assignment Operator
+             Relational Operators
+             Logical Operators
+             Ternary Operator
+             Bitwise Operators
+             Shift Operators
+             instance of operator
+          */
+          
+
+          // Arithmetic Operators
+          //Arithmetic Operators are used to perform simple arithmetic operations on primitive and non-primitive data types. 
+          
+
+          //Unary Operators
+          //Unary Operators need only one operand. They are used to increment, decrement, or negate a value. 
+
+          // Assignment Operator
+        /*
+          ‘=’ Assignment operator is used to assign a value to any variable. It has right-to-left associativity, i.e. value given on the right-hand side of the operator is assigned to the variable on the left, and therefore right-hand side value must be declared before using it or should be a constant. 
+         */
+
+         /*
+          In many cases, the assignment operator can be combined with others to create shorthand compound statements. For example, a += 5 replaces a = a + 5. Common compound
+          */
+
+          //Relational Operator
+          /*
+            Relational Operators
+Relational Operators are used to check for relations like equality, greater than, and less than. They return boolean results after the comparison and are extensively used in looping statements as well as conditional if-else statements.
+           */
+
+           //Logical Operators
+           /*
+            Logical Operators are used to perform “logical AND” and “logical OR” operations, similar to AND gate and OR gate in digital electronics. They have a short-circuiting effect
+            */
+
+            //Ternary operator
+            /*
+             The Ternary Operator is a shorthand version of the if-else statement. It has three operands and hence the name Ternary. The general format is ,
+
+             condition ? if true : if false 
+             */
+
+             //Bitwise Operators
+             /*
+              Bitwise Operators are used to perform the manipulation of individual bits of a number and with any of the integer types. They are used when performing update and query operations of the Binary indexed trees. 
+              */
+
+              //Shift Operators
+              /*
+               Shift Operators are used to shift the bits of a number left or right, thereby multiplying or dividing the number by two, respectively. They can be used when we have to multiply or divide a number by two.
+               */
+
+               // instanceof operator
+               /*
+                The instance of operator is used for type checking. It can be used to test if an object is an instance of a class, a subclass, or an interface. 
+                */
+
+
+
+                //Precedence and Associativity of Java Operators
+
+                /*
+                 Precedence and associative rules are used when dealing with hybrid equations involving more than one type of operator. In such cases, these rules determine which part of the equation to consider first, as there can be many different valuations for the same equation. The below table depicts the precedence of operators in decreasing order as magnitude, with the top representing the highest precedence and the bottom showing the lowest precedence.
+                 */
+    }
+}
